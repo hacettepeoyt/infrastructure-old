d5115480882e88a504980f33f28a4834e2459c7a
div72 <60045611+div72@users.noreply.github.com>
2023-02-08 16:42:42 +0300

    ozguryazilimhacettepe.com: add hucafetaria/huannouncementbot webhooks

---
+++ roles/nginx/templates/tls.conf.js
@@ -0,0 +1,4 @@
+ssl_certificate /etc/letsencrypt/live/{{ master_domain }}/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/{{ master_domain }}/privkey.pem;
+include /etc/letsencrypt/options-ssl-nginx.conf;
+ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
